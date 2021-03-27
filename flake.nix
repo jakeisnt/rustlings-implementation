256e02635576e1fcfeff4046701c0ea875db1c49
Jacob Chvatal <jake@isnt.online>
2021-03-26 21:10:38 -0700

    remove rust-analyzer

--- flake.nix
+++ flake.nix
@@ -1,46 +1,43 @@
 {
   inputs = {
-    nixpkgs-mozilla = { url = github:mozilla/nixpkgs-mozilla; flake = false; };
-    cargo2nix.url = github:onsails/cargo2nix/flake;
-    utils.url = github:numtide/flake-utils;
+    nixpkgs-mozilla = {
+      url = "github:mozilla/nixpkgs-mozilla";
+      flake = false;
+    };
+    cargo2nix.url = "github:onsails/cargo2nix/flake";
+    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, utils, cargo2nix, nixpkgs-mozilla, ... }: utils.lib.eachDefaultSystem (system:
-    let
-      rustChannel = "1.49.0";
-      rustChannelSha256 = "KCh2UBGtdlBJ/4UOqZlxUtcyefv7MH1neoVNV4z0nWs=";
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-          (import "${nixpkgs-mozilla}/rust-overlay.nix")
-          cargo2nix.overlay
-          (
-            self: super: {
-              rustStable = (
-                super.rustChannelOf {
-                  channel = rustChannel;
-                  sha256 = rustChannelSha256;
-                }
-              ).rust;
-            }
-          )
-        ];
-      };
-      rustPkgs = pkgs.rustBuilder.makePackageSet' {
-        inherit rustChannel rustChannelSha256;
-        packageFun = import ./Cargo.nix;
-      };
-    in
-    {
-      defaultPackage = rustPkgs.workspace.rustlings { };
+  outputs = { nixpkgs, utils, cargo2nix, nixpkgs-mozilla, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        rustChannel = "1.49.0";
+        rustChannelSha256 = "KCh2UBGtdlBJ/4UOqZlxUtcyefv7MH1neoVNV4z0nWs=";
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (import "${nixpkgs-mozilla}/rust-overlay.nix")
+            cargo2nix.overlay
+            (self: super: {
+              rustStable = (super.rustChannelOf {
+                channel = rustChannel;
+                sha256 = rustChannelSha256;
+              }).rust;
+            })
+          ];
+        };
+        rustPkgs = pkgs.rustBuilder.makePackageSet' {
+          inherit rustChannel rustChannelSha256;
+          packageFun = import ./Cargo.nix;
+        };
+      in {
+        defaultPackage = rustPkgs.workspace.rustlings { };
 
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          (rustPkgs.workspace.rustlings {})
-          rustStable
-          rust-analyzer
-        ];
-      };
-    }
-  );
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (rustPkgs.workspace.rustlings { })
+            rustStable
+          ];
+        };
+      });
 }
