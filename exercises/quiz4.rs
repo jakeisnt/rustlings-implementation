c7e168e47d6403d1fdab32c2692451b3a00f5dbe
Jacob Chvatal <jake@isnt.online>
2021-03-28 15:40:58 -0700

    quiz4

--- exercises/quiz4.rs
+++ exercises/quiz4.rs
@@ -5,7 +5,31 @@
 
 // Write a macro that passes the quiz! No hints this time, you can do it!
 
-// I AM NOT DONE
+
+// This works, but it's corny. Let's see if we can do better.
+// #[macro_use]
+// mod macros {
+//     macro_rules! my_macro {
+//         ("world!") => {
+//             "Hello world!"
+//         };
+//         ("goodbye!") => {
+//             "Hello goodbye!"
+//         };
+//     }
+// }
+
+
+#[macro_use]
+mod macros {
+    macro_rules! my_macro {
+        // Macros can use other macros - nice!
+        // Can't forget the '$' metavariable references.
+        ($arg:expr) => {
+            format!("Hello {}", $arg)
+        };
+    }
+}
 
 #[cfg(test)]
 mod tests {
