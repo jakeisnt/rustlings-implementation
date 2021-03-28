f045b76e4242435d23f0cb4f7b9154770611fb01
Jacob Chvatal <jake@isnt.online>
2021-03-27 19:25:14 -0700

    move_semantics

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,19 +2,31 @@
 // Make me compile without changing line 13!
 // Execute `rustlings hint move_semantics2` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
+    // Instantiate a new vector
     let vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    // Give mutable reference to vec2 to vec2. vec2 is 'borrowing' vec2
+    let mut vec2 = &vec0;
+
+    // fill_vec takes ownership of the vector made by vec2.to_vec(),
+    // but this is fine because it's a copy rather than the same reference.
+    let mut vec1 = fill_vec(vec2.to_vec());
+
+    // Returns a copy of vec2 with a single element added
+    vec2.to_vec().push(20);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
 
     vec1.push(88);
 
+
+
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+
+
+    println!("{} has length {} content `{:?}`", "vec1", vec2.len(), vec2);
 }
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
--- exercises/move_semantics/move_semantics3.rs
+++ exercises/move_semantics/move_semantics3.rs
@@ -3,13 +3,18 @@
 // (no lines with multiple semicolons necessary!)
 // Execute `rustlings hint move_semantics3` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
     let vec0 = Vec::new();
 
+    // This implicitly creates a copy of the vector.
     let mut vec1 = fill_vec(vec0);
 
+    // Does not work; vec0 has been moved into fill_vec and vec1,
+    // so it can't be accessed outside of the vec1 reference.
+    // println!("{} has length {} content `{:?}`", "vec1", vec0.len(), vec0);
+
+    // vec1 now holds the only allowed reference to vec0,
+    // so it can do whatever with it.
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 
     vec1.push(88);
@@ -17,7 +22,8 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+// Making the argument mutable here allows the vector added to be mutated.
+fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> {
     vec.push(22);
     vec.push(44);
     vec.push(66);
