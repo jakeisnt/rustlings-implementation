f8b78229892dfda0996dbf8af136d803f3dd3fbb
Jacob Chvatal <jake@isnt.online>
2021-03-27 23:45:22 -0700

    errors

--- exercises/error_handling/errorsn.rs
+++ exercises/error_handling/errorsn.rs
@@ -17,19 +17,20 @@
 //
 // Execute `rustlings hint errorsn` for hints :)
 
-// I AM NOT DONE
-
 use std::error;
 use std::fmt;
 use std::io;
 
 // PositiveNonzeroInteger is a struct defined below the tests.
-fn read_and_validate(b: &mut dyn io::BufRead) -> Result<PositiveNonzeroInteger, ???> {
+fn read_and_validate(b: &mut dyn io::BufRead) -> Result<PositiveNonzeroInteger, Box<dyn error::Error>> {
+    // By allowing the type to accept all errors, we get away with this.
+    // It would be better practice to define a custom Box enum that can accept all of the errors
+    // we care about. This post looks like it provides a good overview: https://stevedonovan.github.io/rust-gentle-intro/6-error-handling.html
     let mut line = String::new();
-    b.read_line(&mut line);
-    let num: i64 = line.trim().parse();
-    let answer = PositiveNonzeroInteger::new(num);
-    answer
+    b.read_line(&mut line)?;
+    let num: i64 = line.trim().parse()?;
+    let answer = PositiveNonzeroInteger::new(num)?;
+    Ok(answer)
 }
 
 //
