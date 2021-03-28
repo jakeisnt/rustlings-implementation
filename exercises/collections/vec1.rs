5477d1df1de0ff7dc8ff483a9007db506f96986a
Jacob Chvatal <jake@isnt.online>
2021-03-28 15:28:10 -0700

    vec

--- exercises/collections/vec1.rs
+++ exercises/collections/vec1.rs
@@ -4,11 +4,10 @@
 // Make me compile and pass the test!
 // Execute the command `rustlings hint collections1` if you need hints.
 
-// I AM NOT DONE
-
 fn array_and_vec() -> ([i32; 4], Vec<i32>) {
     let a = [10, 20, 30, 40]; // a plain array
-    let v = // TODO: declare your vector here with the macro for vectors
+    // TODO: declare your vector here with the macro for vectors:
+    let v = vec![10, 20, 30, 40];
 
     (a, v)
 }
@@ -20,6 +19,7 @@ mod tests {
     #[test]
     fn test_array_and_vec_similarity() {
         let (a, v) = array_and_vec();
+        // An array is a slice of a vector!
         assert_eq!(a, v[..]);
     }
 }
